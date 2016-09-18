d73c0e5af059e3d4506fc9ef2549d1f94ef59b18
Kevin Do <kevindokhoale@gmail.com>
2016-09-17 19:53:56 -0700

    Greyscale Filter
    car image example

---
+++ startingFilters.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# greyscale filter OR cv2.0
+
+# IMREAD_COLOR
+# IMREAD_UNCHANGED
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows
