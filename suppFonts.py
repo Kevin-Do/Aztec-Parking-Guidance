02f07bf88354b2532a0b82131c7d2c893a4cdee1
Kevin Do <kevindokhoale@gmail.com>
2016-09-19 09:44:18 -0700

    Reusable list of defaulted fonts

---
+++ suppFonts.py
@@ -0,0 +1,21 @@
+fontList = [
+'FONT_HERSHEY_SIMPLEX',
+'FONT_HERSHEY_PLAIN',
+'FONT_HERSHEY_DUPLEX',
+'FONT_HERSHEY_COMPLEX',
+'FONT_HERSHEY_TRIPLEX',
+'FONT_HERSHEY_COMPLEX_SMALL',
+'FONT_HERSHEY_SCRIPT_SIMPLEX',
+'FONT_HERSHEY_SCRIPT_COMPLEX',
+'FONT_ITALIC',
+];
+
+#FONT_HERSHEY_SIMPLEX 	normal size sans-serif font
+#FONT_HERSHEY_PLAIN 	small size sans-serif font
+#FONT_HERSHEY_DUPLEX 	normal size sans-serif font (more complex than FONT_HERSHEY_SIMPLEX)
+#FONT_HERSHEY_COMPLEX 	normal size serif font
+#FONT_HERSHEY_TRIPLEX 	normal size serif font (more complex than FONT_HERSHEY_COMPLEX)
+#FONT_HERSHEY_COMPLEX_SMALL 	smaller version of FONT_HERSHEY_COMPLEX
+#FONT_HERSHEY_SCRIPT_SIMPLEX 	hand-writing style font
+#FONT_HERSHEY_SCRIPT_COMPLEX 	more complex variant of FONT_HERSHEY_SCRIPT_SIMPLEX
+#FONT_ITALIC    italics style font
